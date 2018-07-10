fac6beb852bcc140f0d95b9dbb3903e69617b82b
maciejcieslar <marekmckl@gmail.com>
2018-07-10 18:26:29 +0200

    fixes

--- src/common/browser/api/find-posts.ts
+++ src/common/browser/api/find-posts.ts
@@ -1,28 +1,25 @@
 import { FindPosts } from 'src/common/interfaces';
 
-const findPosts: FindPosts = async function findPosts(
-  hashtag,
-  numberOfPosts = 12,
-) {
-  return this.getPage(`/explore/tags/${hashtag}`, async (page) => {
+const findPosts: FindPosts = async function findPosts(hashtag, numberOfPosts = 12) {
+  return this.getPage(`/explore/tags/${hashtag}`, async page => {
     if (numberOfPosts > 12) {
-      await page.evaluate((posts) => {
+      await page.evaluate(async posts => {
         const { scraper } = window as any;
 
-        scraper.scrollPageTimes({ times: Math.ceil((posts - 9) / 12) });
+        await scraper.scrollPageTimes({ times: Math.ceil((posts - 12) / 12) + 1 });
       }, numberOfPosts);
     }
 
     // waitFor render
-    await page.waitFor(1000);
+    await page.waitFor(10000);
 
-    return page.evaluate((posts) => {
+    return page.evaluate(posts => {
       const { scraper } = window as any;
 
       return scraper
         .find({ selector: 'a[href^="/p/"]', count: posts + 9 })
         .slice(9)
-        .map((el) => el.getAttr('href'));
+        .map(el => el.getAttr('href'));
     }, numberOfPosts);
   });
 };
